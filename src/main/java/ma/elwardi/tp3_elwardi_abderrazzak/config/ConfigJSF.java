2efa96f4c6f96470683036d2bf22b117fbc0a82d
a.elwardi <a.elwardi98gmail.Com>
2022-12-17 20:11:49 +0100

    Ajout des pages et templates.

---
+++ src/main/java/ma/elwardi/tp3_elwardi_abderrazzak/config/ConfigJSF.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.elwardi.tp3_elwardi_abderrazzak.config;
+
+import jakarta.faces.annotation.FacesConfig;
+import jakarta.faces.bean.ApplicationScoped;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
---
+++ src/main/java/ma/elwardi/tp3_elwardi_abderrazzak/jsf/Bean.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package ma.elwardi.tp3_elwardi_abderrazzak.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "bean")
+@RequestScoped
+public class Bean {
+
+    private int nombre;
+
+    public String afficher() {
+        return "affichage_3?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+}
